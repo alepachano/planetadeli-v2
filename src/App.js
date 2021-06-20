814d2ab67637e9aadc40f4ef7a5ff8ebc793a931
avpachano <av.pachano@gmail.com>
2021-06-20 13:36:19 -0400

    se crea un componente navbar y se importa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import NavBar from './components/NavBar/index.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo! soy Ale y voy a aprender a programar con react js.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavBar />
   );
 }
 
