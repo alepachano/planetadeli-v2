62fb2cc583460c927d12a6fcea23ae9f3abce7ee
avpachano <av.pachano@gmail.com>
2021-06-30 18:12:55 -0400

    se crea ItemCountComponent

--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,7 +1,10 @@
+import ItemCountComponent from "../../components/ItemCountComponent";
+
 function ItemListContainer( {greeting} ) {
     return (
         <div>
             <h2>Greeting: {greeting}</h2>
+            < ItemCountComponent />
         </div>
     )
 }
