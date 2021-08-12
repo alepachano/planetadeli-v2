aded14ce0927920c822d8762f6b057f078e7062f
avpachano <av.pachano@gmail.com>
2021-08-11 22:04:09 -0400

    se ingresa componente para error 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import error404 from './error404.jpg';
+
 import { Navbar } from './components/NavbarComponent';
 import { ItemListContainer } from './containers/ItemListContainer';
 import { ItemDetailContainer } from './containers/ItemDetailContainer';
@@ -8,6 +8,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import { CartComponent } from './components/CartComponent';
 import { Footer } from './components/FooterComponent';
+import { NotFoundComponent } from './components/NotFoundComponent';
 
 function App() {
   return (
@@ -33,8 +34,9 @@ function App() {
               <CartComponent />
             </Route>
 
-            {/* TO DO: hacer componente not found */}
-            <Route path={"*"} component={() => <div className="text-center"><img src={error404} className="error404" alt={'not-found'} /></div>} />
+            <Route path={"*"}>
+              <NotFoundComponent />
+            </Route>
           </Switch>
 
           <Footer />
